807e5da682294edfa5cb79132bb2b7433cca9332
PieSeek <risto.richter@mailbox.org>
2024-03-13 17:11:30 +0200

    4.1 Hinnete tabel failist

--- 4.1 Hinnete tabel failist/kood.py
+++ 4.1 Hinnete tabel failist/kood.py
@@ -1,12 +1,20 @@
+import csv
+
 fail = open("4.1 Hinnete tabel failist/hinded.csv", encoding="UTF-8")
+loetudFail = csv.reader(fail, delimiter=',')
 
 hinnete_tabel = []
 
-for rida in fail:
-    for hinne in rida:
-        hinnete_tabel.append(hinne)
-        
+for rida in loetudFail:
+    hinnete_tabel.append(rida)
 
 fail.close()
 
-print(hinnete_tabel)
+
+for rida in hinnete_tabel:
+    hinded = []
+    aine = rida[0]
+    rida.pop(0)
+    for hinne in rida:
+        hinded.append(int(hinne))
+    print(aine + ':', str(round(sum(hinded) / len(hinded), 1)))
